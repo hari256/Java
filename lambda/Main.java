72ec88b6f0d3ec70703b118efc84748e96c2ca3a
hari256 <hariprasanthsh@gmail.com>
2024-10-02 16:26:55 +0530

    lambda

--- lambda/Main.java
+++ lambda/Main.java
@@ -2,7 +2,14 @@ package lambda;
 
 public class Main {
     public static void main(String ar[]){
-        Example example = new Example();
-        example.test("Testing lambda");
+        /*Example example = new Example();
+        example.test("Testing lambda");*/
+
+        //lambda -> Here there in no need to implement interface in Example.java because we have provided the implementation directly
+        Book lambdaBook = a -> System.out.print(a);
+        testThing(lambdaBook);
+    }
+    static void  testThing(Book lambdaBook){
+        lambdaBook.test("Hello lambda!");
     }
 }
